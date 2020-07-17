a5b3a6d2f4148c5dae968f6ae72943f766987344
Avilagutier <mario.avila_gutierrez@protonmail.com>
2020-07-17 18:04:29 +0200

    First exercise Hacherrank

---
+++ return_ex.py
@@ -0,0 +1,34 @@
+'''
+ Return me permite hacer un retorno del valor que se empleo en una funcion
+ segun : openbookproject es " Retorne inmediatamente de esta funcion y use la
+ siguiente expresion como un valor de retorno"
+ '''
+
+# def add(a,b):
+    # return a + b
+
+'''
+    Esto hara que al definir mis variables en def, me devuelva el valor,
+    en este caso la suma de a + b
+'''
+
+
+'''
+    problema a resolver :
+
+    int solveMeFirts(int a,int b)
+
+    a is the first integer input
+    b is the second integer input
+
+    return values
+
+    sum of the above two integers
+
+    a = 2
+    b = 3
+'''
+def solveMeFirst(a,b):
+    return a + b
+
+
